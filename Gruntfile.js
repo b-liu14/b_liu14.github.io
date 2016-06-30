0ad326e55b4e9e475f24deda1c9e82871d417452
bin liu <bin liu>
2016-07-01 01:41:58 +0800

    html min

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,47 +5,63 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    		//压缩css
-           cssmin: {
-               //文件头部输出信息
-               options: {
-                   banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-                   //美化代码
-                   beautify: {
-                       //中文ascii化，非常有用！防止中文乱码的神配置
-                       ascii_only: true
-                   }
-               },
-               my_target: {
-                   files: [
-                       {
-                           expand: true,
-                           //相对路径
-                           cwd: 'css/',
-                           src: '*.css',
-                           dest: 'css/',
-                           rename: function (dest, src) {
-                                   var folder = src.substring(0, src.lastIndexOf('/'));
-                                   var filename = src.substring(src.lastIndexOf('/'), src.length);
-                                   //  var filename=src;
-                                   filename = filename.substring(0, filename.lastIndexOf('.'));
-                                   var fileresult=dest + folder + filename + '.css';
-                                   grunt.log.writeln("现处理文件："+src+"  处理后文件："+fileresult);
+		//压缩css
+       cssmin: {
+         //文件头部输出信息
+         options: {
+             banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+             //美化代码
+             beautify: {
+                 //中文ascii化，非常有用！防止中文乱码的神配置
+                 ascii_only: true
+             }
+         },
+         my_target: {
+             files: [
+              {
+               expand: true,
+               //相对路径
+               cwd: 'css/',
+               src: '*.css',
+               dest: 'css/',
+               rename: function (dest, src) {
+                       var folder = src.substring(0, src.lastIndexOf('/'));
+                       var filename = src.substring(src.lastIndexOf('/'), src.length);
+                       //  var filename=src;
+                       filename = filename.substring(0, filename.lastIndexOf('.'));
+                       var fileresult=dest + folder + filename + '.css';
+                       grunt.log.writeln("现处理文件："+src+"  处理后文件："+fileresult);
 
-                                   return fileresult;
-                                 //return  filename + '.min.js';
-                                   }
+                       return fileresult;
+                     //return  filename + '.css';
                        }
-                   ]
-               }
+                 }
+             ]
            }
+    },
+
+    // 压缩html
+    htmlmin: {                                       // Task
+      dist: {                                      // Target
+        options: {                                 // Target options
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files:[{
+          expand: true,
+          cwd: './',
+          src: '**/*.html',
+          dest: './'
+      }]
+      }
+    }
 
   });
 
   // 告诉grunt我们将使用插件
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
   // 告诉grunt当我们在终端中输入grunt时需要做些什么
-  grunt.registerTask('default', ['cssmin']);
+  grunt.registerTask('default', ['cssmin', 'htmlmin']);
 
 };
